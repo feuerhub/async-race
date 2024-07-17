c4f7564f6689a550962e7dfb741760754c10fa62
Mikhail Markov <mikelmmv@gmail.com>
2024-07-17 16:28:26 +0500

    feat: add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
-import { Header } from "./components/header/Header";
+import { Routes, Route } from "react-router-dom";
+
+import { Header } from "./components";
+import { Garage, Winners } from "./views";
 
 export function App() {
   return <>
     <Header />
+    <Routes>
+      <Route path="/garage" element={<Garage />} />
+      <Route path="/winners" element={<Winners />} />
+    </Routes>
   </>;
 }
---
+++ src/views/index.ts
@@ -0,0 +1,4 @@
+import { Garage } from "./Garage";
+import { Winners } from "./Winners";
+
+export { Garage, Winners };
